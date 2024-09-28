e2b05e52efa135b89422e024bfb115789ade6ae6
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2024-09-28 16:45:18 +0300

    Add files via upload

---
+++ js/hw.js
@@ -0,0 +1,29 @@
+const age = 13;
+console.log(age);
+const myName = "Даня";
+console.log(myName);
+const isStudent = true;
+console.log(isStudent);
+const myString = " з вашої улюбленою цитатою";
+console.log(myString);
+let myNumber = 8;
+console.log(myNumber);
+myNumber += 10;
+console.log(myNumber);
+const myNull = null;
+console.log(myNull);
+let youName = prompt("Ваше ім'я");
+console.log("Привіт", youName);
+let myConf =  confirm("Ви підтверджуєте дію");
+if (myConf === true){
+console.log("Дякую за підтвердження!");
+} else {
+    console.log("Дію відмінено!");
+}
+alert("Дія небезпечна");
+let myConfe =  confirm("Ви підтверджуєте дію");
+if (myConfe === true){
+console.log("Дякую за підтвердження!");
+} else {
+    console.log("Дію відмінено!");
+}
